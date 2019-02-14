d51690e56750cc2390df2d38d5fc2139918059cb
Daniela Baron <baron.daniela@gmail.com>
2019-02-13 19:31:56 -0500

    displaying redux sate inside our components

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-        <h1>PhotoGrid Component TODO</h1>
+        {this.props.posts.map((post, i) => (
+          <Photo {...this.props} key={i} i={i} post={post} />
+        ))}
       </div>
     );
   },
